f870f090cb2658c98ba1bc75443ea35e49bc7341
yangdefeng <defeng.yang@skilnets.com>
2021-10-27 18:15:08 +0800

    commit

--- auto_restart/auto_restart_when_git_changed.py
+++ auto_restart/auto_restart_when_git_changed.py
@@ -18,8 +18,9 @@ script_name = command_args.script_name
 
 
 def restart(args=None):
-    # kill_str = f'''git pull && ps -aux|grep {script_name} |grep -v grep|awk '{{print $2}}' |xargs kill -9;'''
-    kill_str = f'''ps auxww | grep '{script_name} ' | awk '{{print $2}}' | xargs kill'''
+    os.system('git pull')
+    kill_str = f'''ps -aux|grep {script_name} |grep -v grep|grep -v auto_restart_when_git_changed |awk '{{print $2}}' |xargs kill -9;'''
+    # kill_str = f'''ps auxww | grep '{script_name} ' | awk '{{print $2}}' | xargs kill'''
     # subprocess.getstatusoutput('git pull')
     print('00000')
     # os.system(kill_str)
